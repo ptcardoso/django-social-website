8796a92ccade9ca2dbf8de2af766b5fc4293e73b
Pedro Cardoso <pmtcardoso@gmail.com>
2019-03-28 21:53:27 +0000

    Add password change form

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
-from django.contrib.auth.views import LoginView, LogoutView, logout_then_login
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView, logout_then_login
 from . import views
 
 app_name = 'account'
 urlpatterns = [
     # url(r'^login/$', views.user_login, name='login')
+    url(r'^password-change/$', PasswordChangeView.as_view(), name='password_change'),
+    url(r'^password-change/done/$', PasswordChangeDoneView.as_view(), name='password_change_done'),
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^logout/$', LogoutView.as_view(), name='logout'),
     url(r'^logout-then-login/$', logout_then_login, name='logout_then_login'),
